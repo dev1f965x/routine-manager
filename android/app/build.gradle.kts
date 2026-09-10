677fc6b50d5875543e908b487c2ed766f8331b7f
dev1f965x <315394198+dev1f965x@users.noreply.github.com>
2026-09-10 15:35:04 +0900

    🔧 chore: change the package identifier to io.github.dev1f965x
    Without an owned domain, io.github.<account>, backed by github.io, is the convention.
    com.<account> is a name we don't own and could collide with someone else's.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.dev1f965x.routine_manager"
+    namespace = "io.github.dev1f965x.routine_manager"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.dev1f965x.routine_manager"
+        applicationId = "io.github.dev1f965x.routine_manager"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/io/github/dev1f965x/routine_manager/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/dev1f965x/routine_manager/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dev1f965x.routine_manager
+package io.github.dev1f965x.routine_manager
 
 import io.flutter.embedding.android.FlutterActivity
 
